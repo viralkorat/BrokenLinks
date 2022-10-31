0792f4680a3ad854e3711e1caa1a990892abc1bb
vkorat <vkorat@CATOR-VKORAT1.armstrong.local>
2022-10-31 10:15:43 -0400

    How to find Broken Links

---
+++ src/test/java/InputWithoutSendkeys.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InputWithoutSendkeys {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+		WebElement email = driver.findElement(By.xpath("//input[@id='email']"));
+		WebElement password = driver.findElement(By.xpath("//input[@id='pass']"));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("arguments[0].value='Viral@gmail.com', arguments[1].value='Test@1234'", email, password);
+
+	}
+}
